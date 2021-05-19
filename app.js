fa5b86640a24e19db2d233324e082ef408ea858a
rafaelsuarez <rafaelaugustosc@gmail.com>
2021-05-18 23:31:39 -0400

    post updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose')
 const bodyParser= require('body-parser')
 const express = require('express')
 const app=express()
-const port = process.env.PORT ||4000
+const port = process.env.PORT
 
 const customerRoutes= require('./Routes/customer')
 
